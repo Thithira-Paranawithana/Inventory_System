1b2f61ba77de38a252b1203fc0d7840fe93de8c5
Thithira-Paranawithana <153026117+Thithira-Paranawithana@users.noreply.github.com>
2025-08-24 21:08:24 +0530

    InventoryDbContext file creation and database connection

---
+++ Inventory_System/Inventory_System/Data/InventoryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Inventory_System.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_System.Data
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<SalesTransaction> SalesTransactions { get; set; }
+        public DbSet<ReorderRecommendation> ReorderRecommendations { get; set; }
+
+    }
+}
--- Inventory_System/Inventory_System/obj/Debug/net9.0/Inventory_System.AssemblyInfo.cs
+++ Inventory_System/Inventory_System/obj/Debug/net9.0/Inventory_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e5ac59cb93c7fd80630fc8eae61c16be5b9a1f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
