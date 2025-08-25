f1bd5849c1dd44c5ea3dbbd6e954dda80b22a50c
Thithira-Paranawithana <153026117+Thithira-Paranawithana@users.noreply.github.com>
2025-08-26 01:06:26 +0530

    JWT, cookie  config, adding policies

--- Inventory_System/Inventory_System/Program.cs
+++ Inventory_System/Inventory_System/Program.cs
@@ -1,7 +1,10 @@
 using Inventory_System.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,97 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<InventoryDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Auth config
+builder.Services.AddAuthentication(options =>
+{
+    // default JWT bearer scheme for endpoints
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    // config parameters for token validation
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true, // verify token is signed with secret key
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!)),
+
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        ValidateLifetime = true,  // check whether token is expired
+        ClockSkew = TimeSpan.Zero // no tolerance for expired tokens
+    };
+
+    // allow to read JWT token from cookies as well
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            // if there is no auth header check for token in cookie
+            if (string.IsNullOrEmpty(context.Token) && context.Request.Cookies.ContainsKey("AccessToken"))
+            {
+                context.Token = context.Request.Cookies["AccessToken"];
+            }
+            return Task.CompletedTask;
+        }
+    };
+})
+.AddCookie("Cookies", options =>
+{
+    // cookie config for web login
+    options.Cookie.Name = "InventoryAuth";
+    options.LoginPath = "/login"; // redirect if not authenticated
+    options.LogoutPath = "/logout";
+    options.AccessDeniedPath = "/access-denied"; // redirect if access is denied
+    options.ExpireTimeSpan = TimeSpan.FromHours(1); // cookie expire time is 1 hr
+    options.SlidingExpiration = true; // refresh expire time on each request
+
+    // security settings for cookies
+    options.Cookie.HttpOnly = true; // XSS protection
+    options.Cookie.SameSite = SameSiteMode.Strict; // CSRF protection
+    options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest; // HTTPS
+
+    // return 401 without redirecting to login page
+    options.Events.OnRedirectToLogin = context =>
+    {
+        if (context.Request.Path.StartsWithSegments("/api"))
+        {
+            context.Response.StatusCode = 401; // unauthorized
+            return Task.CompletedTask;
+        }
+        context.Response.Redirect(context.RedirectUri);
+        return Task.CompletedTask;
+    };
+});
+
+
+builder.Services.AddAuthorization(options =>
+{
+    // for API endpoints, token is required
+    options.AddPolicy("ApiPolicy", policy =>
+    {
+        policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+        policy.RequireAuthenticatedUser();
+    });
+
+    // web needs cookie authentication
+    options.AddPolicy("WebPolicy", policy =>
+    {
+        policy.AuthenticationSchemes.Add("Cookies");
+        policy.RequireAuthenticatedUser();
+    });
+
+    // for managers only
+    options.AddPolicy("ManagerOnly", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireRole("StoreManager");
+    });
+
+});
 
 
 var app = builder.Build();
@@ -25,7 +119,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseAuthentication(); // checks jwt, cookies
+app.UseAuthorization(); // check whether user has permission
 
 app.MapControllers();
 
